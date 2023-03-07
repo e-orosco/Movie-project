145bf217d09a168914c8a5272fe5f3c8ff363b3c
Esteban Orosco <eorosco360@gmail.com>
2023-03-06 21:31:00 -0500

    Adding Movie model,serializer and  swagger

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .models import Movie
+from .serializer import MovieSerializer
+from rest_framework import viewsets
+#from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+#    permission_classes = [IsAuthenticated]
